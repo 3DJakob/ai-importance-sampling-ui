088928f4ab647a3abb5325abcb4bf85b4627c69c
Jakob Unnebäck <jakob@folkdatorn.se>
2023-02-15 15:16:25 +0100

    Update landing page design

--- src/pages/Networks.tsx
+++ src/pages/Networks.tsx
@@ -1,12 +1,38 @@
 import React from 'react'
 import { useCollection } from 'react-firebase-hooks/firestore'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import NetworkInfo from '../components/NetworkInfo'
 import collections from '../lib/collections'
+import styled from 'styled-components'
+import bg from '../assets/bg.webp'
+import { Network } from '../lib/types'
+
+// load bg.webp from assets folder
+const Background = styled.div`
+  background-image: url(${bg});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+`
+
+const Button = styled.div`
+  cursor: pointer;
+  transition: 0.2s;
+  margin: 20px;
+
+  :hover {
+    background-color: rgba(255, 255, 255, 0.2);
+  }
+`
 
 const Networks: React.FC = () => {
   const [networksFirebase, loading, error] = useCollection(collections.networks)
   const networks = networksFirebase?.docs.map(doc => doc.data())
+  const navigate = useNavigate()
 
   if (networks == null || loading) {
     return <div>Loading...</div>
@@ -16,14 +42,18 @@ const Networks: React.FC = () => {
     return <div>Error: {String(error)}</div>
   }
 
+  const handlePress = (network: Network): void => {
+    navigate(`/network/${network.name}`)
+  }
+
   return (
-    <div>
+    <Background>
       {networks.map(network => (
-        <Link key={network.name} to={'/network/' + network.name}>
+        <Button key={network.name} onClick={() => handlePress(network)}>
           <NetworkInfo network={network} />
-        </Link>
+        </Button>
       ))}
-    </div>
+    </Background>
   )
 }
 
